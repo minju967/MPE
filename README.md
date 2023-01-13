a72c2a38cc37c0e7358e4b5de244613156e7acd4
Minju <minj0329@gmail.com>
2023-01-13 15:15:58 +0900

    #

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # MPE
+
+Create_datafolder.py
+    
+    <데이터셋 생성 파일>
+
+    '''
+        DATASET
+        |
+        |--- dataset_1
+            |
+            |--- Train
+            |      |
+            |      |--- Images
+            |      |        |
+            |      |        |---img1.png
+            |      |        |---img2.png
+            |      |        |---img3.png
+            |      |--- label.csv
+            |--- Test
+                    |
+                    |--- Images
+                    |        |
+                    |        |---img1.png
+                    |        |---img2.png
+                    |        |---img3.png
+                    |--- label.csv
+    '''
+
+    클래스: 연삭전극, 고속가공, 고속전극, 연삭, 와이어, None 
+    label:  A, HSP, C, Grinding, Wire, None
+
+    클래스간의 데이터 imbalance가 크기 때문에 Random하게 train/test를 split하게 되면 비율이 맞지 않다.
+    테스트 데이터에 없는 클래스가 있을 수 있다.
+    따라서, 각 클래스 별 데이터를 8:2 비율로 train/test로 분리된다.
+
+    데이터의 label은 Multi-label을 가지고 있기 때문에 test로 분리된 이미지는 뒤에 클래스에서 다시 선택될 수 있다.
+    따라서 train / test 데이터 비율이 8:2가 안될 수도 있다. 
+
+Separate_image
+
+    <이미지 클래스 분류>
+
+    Create_datafolder.py를 실행하기 위한 base datafolder 생성
--- create_datafolder.py
+++ create_datafolder.py
@@ -5,7 +5,34 @@ import random
 import pandas as pd
 import glob
 import os
-    
+
+
+
+def Separate_image(opt):
+    # label_df = pd.read_csv(opt.csv)
+
+
+    label_df = pd.DataFrame(data=[[0, 'A123.png', 0,1,1,0,0,1,'C:\\home', True]], columns=['Idx', 'Name', 'A', 'HSM', 'C', 'Grinding', 'Wire', 'None', 'Path', 'Labeled'])
+    unlabeled_path = None
+
+    for idx in range(len(label_df.index)):
+        row = label_df.iloc[idx]        # 행번호로 불러오기
+        img_path = row['Path']
+        img_label = list(row['A':'None'].to_list())
+        _labeled = row['Labeled']
+
+        if _labeled:
+            for i in range(6):
+                cls_path = None
+                if img_label[i]:
+                    pass
+                else:
+                    shutil.copyfile(src=img_path, dst=cls_path)
+                    pass
+        else:
+            shutil.copyfile(src=img_path, dst=unlabeled_path)
+
+    return
 
 def create_datafolder(path):
     folders = os.listdir(path)  # Data folder에 들어있는 파일 리스트
@@ -26,35 +53,14 @@ def create_datafolder(path):
 
     return os.path.join(path, folder_name)
 
-def main(opt):
-    '''
-        DATASET
-        |
-        |--- dataset_1
-            |
-            |--- Train
-            |      |
-            |      |--- Images
-            |      |        |
-            |      |        |---img1.png
-            |      |        |---img2.png
-            |      |        |---img3.png
-            |      |--- label.csv
-            |--- Test
-                    |
-                    |--- Images
-                    |        |
-                    |        |---img1.png
-                    |        |---img2.png
-                    |        |---img3.png
-                    |--- label.csv
-    '''
+def create_dataset(opt):
+
     random.seed(datetime.now().timestamp())
 
     save_df_path = create_datafolder(opt.save)
-    All_df = pd.read_csv('.\\*.csv')
-    train_df = pd.DataFrame(index=range(0,0), columns=['Idx', 'Name', 'A', 'HSM', 'C', 'Grinding', 'Wire', 'None', 'Path'])
-    test_df = pd.DataFrame(index=range(0,0), columns=['Idx', 'Name', 'A', 'HSM', 'C', 'Grinding', 'Wire', 'None', 'Path'])
+    All_df = pd.read_csv(opt.csv)
+    train_df = pd.DataFrame(index=range(0,0), columns=['Idx', 'Name', 'A', 'HSM', 'C', 'Grinding', 'Wire', 'None', 'Path', 'Labeled'])
+    test_df = pd.DataFrame(index=range(0,0), columns=['Idx', 'Name', 'A', 'HSM', 'C', 'Grinding', 'Wire', 'None', 'Path', 'Labeled'])
 
     # class_list = os.listdir(opt.path)
     class_list = ['A', 'B', 'C', 'D', 'E']
@@ -107,8 +113,11 @@ def main(opt):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--path', type=str, default='D:\\VS_CODE\\Paper\\Data', help="folder_name")
-    parser.add_argument('--save', type=str, default='D:\\VS_CODE\\Paper\\Datafolder', help="Save folder")
+    parser.add_argument('--path', type=str, default='D:\\VS_CODE\\Paper\\Data', help="folder_name")                     # Data folder에는 클래스별 데이터
+    parser.add_argument('--save', type=str, default='D:\\VS_CODE\\Paper\\Datafolder', help="Save folder")               # Datafolder folder에는 학습/평가에 사용될 데이터셋 폴더
+    parser.add_argument('--csv', type=str, default='D:\\VS_CODE\\Paper\\Data\\label.csv', help="data_label.csv")
     args = parser.parse_args()
     
-    main(args.path)
+    Separate_image(args.csv)
+    # create_dataset(args.path)
+    
